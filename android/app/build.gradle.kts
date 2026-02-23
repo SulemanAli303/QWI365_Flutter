c7a5e66df89ba18afe8447111866ec39f7078730
Suleman Ali <Suleman.ali303@gmail.com>
2026-02-23 20:43:12 +0500

    QWI build updated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,7 +23,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "in.realtech.qwi365.qwi365"
+    namespace = "in.realtech.qwi365"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -39,7 +39,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "in.realtech.qwi365.qwi365"
+        applicationId = "in.realtech.qwi365"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
